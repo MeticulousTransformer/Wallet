d5dc3f571265134bd843dd960c77fc0968b0629d
Hanzo <82519487+MeticulousTransformer@users.noreply.github.com>
2024-02-08 02:28:52 +0400

    Update makeAccount.js

--- Wallet/Bank/makeAccount.js
+++ Wallet/Bank/makeAccount.js
@@ -1,7 +1,34 @@
 import Bank from "./bankAccount";
+const fs = require("fs");
 
-//TO make loggin and create wallet button,
+function register() {
+  var username = document.getElementById("regUsername").value;
+  var password = document.getElementById("regPassword").value;
+  // Perform validation if needed
+  // For simplicity, we're not doing validation here
+  const newObject = new Bank(username, password);
 
-//TO add and remove funds
+  // Save the username and password in JSON (mini database for now)
 
-//To show balance
+  // Display sign-in form
+  document.getElementById("registerForm").style.display = "none";
+  document.getElementById("signInForm").style.display = "block";
+}
+
+function signIn() {
+  var username = document.getElementById("signInUsername").value;
+  var password = document.getElementById("signInPassword").value;
+  // Perform validation if needed
+  // For simplicity, we're not doing validation here
+
+  // Retrieve password from localStorage
+  var storedPassword = localStorage.getItem(username);
+
+  // Check if the stored password matches the input password
+  if (storedPassword === password) {
+    alert("Sign In Successful!");
+    // Redirect to another page or perform other actions
+  } else {
+    alert("Incorrect Username or Password");
+  }
+}
